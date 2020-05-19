8e46f8e692b3b652e12fcdf7ad8960e6ce928a4b
0xboz <0xboz@hacari.com>
2020-05-19 19:15:18 -0400

    Updated packages' versions, contact info, and app name;

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from setuptools import setup
 import os
 
 setup(
-    name='django-dynamic-scraper',
-    version='0.13.2',
-    description='Creating Scrapy scrapers via the Django admin interface',
-    author='Holger Drewes',
-    author_email='Holger.Drewes@gmail.com',
-    url='https://github.com/holgerd77/django-dynamic-scraper/',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    license='BSD License',
+    name='scrapy-django-dashboard',
+    version='1.0',
+    description='Scrapy Spiders Over Django Admin Dashboard – A Fork From Django-Dynamic-Scraper',
+    author='0xboz',
+    author_email='0xboz@hacari.com',
+    url='https://github.com/0xboz/scrapy_django_dashboard',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    license='MIT',
     platforms=['OS Independent'],
     packages=[
-        'dynamic_scraper',
-        'dynamic_scraper.management',
-        'dynamic_scraper.management.commands',
-        'dynamic_scraper.migrations',
-        'dynamic_scraper.south_migrations',
-        'dynamic_scraper.spiders',
-        'dynamic_scraper.utils',
+        'scrapy_django_dashboard',
+        'scrapy_django_dashboard.management',
+        'scrapy_django_dashboard.management.commands',
+        'scrapy_django_dashboard.migrations',
+        'scrapy_django_dashboard.south_migrations',
+        'scrapy_django_dashboard.spiders',
+        'scrapy_django_dashboard.utils',
     ],
     package_data = {
-        'dynamic_scraper': [
+        'scrapy_django_dashboard': [
             'static/js/*',
         ],
     },
@@ -30,25 +30,26 @@ setup(
     #    Django, Scrapy and Celery requirements are commented out here and have
     #    to be installed manually to avoid side-effects when updating the software.
     #    Version numbers are updated accordingly though.
-    #    'Django>=1.11,<1.12',
-    #    'Scrapy>=1.5,<1.6',
-    #    'scrapy-djangoitem>=1.1.1,<1.2',
-    #    'scrapy-splash>=0.7,<0.8', # Optional
-    #    'scrapyd>=1.2,<1.3',
-        'jsonpath-rw>=1.4',
+    #    'Django==3.0.6',
+    #    'Scrapy==2.1.0',
+    #    'scrapy-djangoitem==1.1.1',
+    #    'scrapy-splash==0.7.2', # Optional
+    #    'scrapyd==1.2.1',
+        'jsonpath-rw==1.4.0',
+    #    Use kombu/celery/django-celery in the root instead
     #    'kombu>=3.0.37,<3.1',
     #    'Celery==3.1.25',
     #    'django-celery==3.2.1', # Scheduling
-        'future>=0.17,<0.18',
-        'pillow>=5.0,<6.0',
-        'attrs>=17.4.0',
+        'future==0.17.1',
+        'pillow==5.4.1',
+        'attrs==19.3.0',
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Utilities',
