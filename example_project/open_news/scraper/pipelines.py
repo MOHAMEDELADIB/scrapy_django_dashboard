6c158a51c9e0a9e77dddf57ff69cfdddd09587d0
0xboz <0xboz@hacari.com>
2020-05-24 16:21:11 -0400

    Formatting;

--- example_project/open_news/scraper/pipelines.py
+++ example_project/open_news/scraper/pipelines.py
@@ -6,26 +6,27 @@ from django.db.utils import IntegrityError
 from scrapy.exceptions import DropItem
 from scrapy_django_dashboard.models import SchedulerRuntime
 
+
 class DjangoWriterPipeline(object):
-    
+
     def process_item(self, item, spider):
         if spider.conf['DO_ACTION']:
             try:
                 item['news_website'] = spider.ref_object
-                
+
                 checker_rt = SchedulerRuntime(runtime_type='C')
                 checker_rt.save()
                 item['checker_runtime'] = checker_rt
-                
+
                 item.save()
-                spider.action_successful = True                
+                spider.action_successful = True
                 spider.logger.info("{cs}Item {id} saved to Django DB.{ce}".format(
                     id=item._id_str,
                     cs=spider.bcolors['OK'],
                     ce=spider.bcolors['ENDC']))
-                    
+
             except IntegrityError as e:
                 spider.logger.error(str(e))
                 raise DropItem("Missing attribute.")
-                
+
         return item
